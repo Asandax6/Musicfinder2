a8049338fde99dc5bcbed159c9c11b8ceacd1081
Asandax6 <asandax6@gmail.com>
2020-07-09 20:28:18 +0200

    first commit

---
+++ example/android/app/src/main/java/com/asanda/musicfinder2example/MainActivity.java
@@ -0,0 +1,14 @@
+package com.asanda.musicfinder2example;
+
+import android.os.Bundle;
+
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    GeneratedPluginRegistrant.registerWith(this);
+  }
+}
